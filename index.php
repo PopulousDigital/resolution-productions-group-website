c1a618b4d34c15332c0421d06fd864e6f2ccfd78
Chris Vuletich <cvuletich@gmail.com>
2015-05-08 14:19:20 -0500

    Adding apple touch icons

--- index.php
+++ index.php
@@ -18,10 +18,10 @@
 	    <script src="js/respond.min.js"></script>
     <![endif]-->
     <link rel="shortcut icon" href="img/favicon.png">
-    <link rel="apple-touch-icon-precomposed" sizes="144x144" href="images/ico/apple-touch-icon-144-precomposed.png">
-    <link rel="apple-touch-icon-precomposed" sizes="114x114" href="images/ico/apple-touch-icon-114-precomposed.png">
-    <link rel="apple-touch-icon-precomposed" sizes="72x72" href="images/ico/apple-touch-icon-72-precomposed.png">
-    <link rel="apple-touch-icon-precomposed" href="images/ico/apple-touch-icon-57-precomposed.png">
+    <link rel="apple-touch-icon-precomposed" sizes="144x144" href="img/apple-touch-icon-144-precomposed.png">
+    <link rel="apple-touch-icon-precomposed" sizes="114x114" href="img/apple-touch-icon-114-precomposed.png">
+    <link rel="apple-touch-icon-precomposed" sizes="72x72" href="img/apple-touch-icon-72-precomposed.png">
+    <link rel="apple-touch-icon-precomposed" href="img/apple-touch-icon-57-precomposed.png">
   </head>
 <body>
 	<!-- Page Loader -->
