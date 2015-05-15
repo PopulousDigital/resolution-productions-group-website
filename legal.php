ddcb2fe148bd8215175f5b6a58d33ff790df4acf
Chris Vuletich <cvuletich@gmail.com>
2015-05-15 16:12:16 -0500

    Fixing legal copy formatting

--- legal.php
+++ legal.php
@@ -12,7 +12,7 @@
 				<div class="col-sm-8 col-sm-offset-2">
           <h1>Legal</h1>
           <p>Resolution Productions Group is an assumed name of Ops 3 LLC. If you have questions or comments about this website, its content, our services, or other information concerning our business operations, you may use our "contact" interface or write or call us:</p>
-          <p>Ops 3 LLC<br />2226 W. Walnut Chicago, IL 60612<br />312.243.8230</p>
+          <p>Ops 3 LLC<br />2226 W. Walnut<br />Chicago, IL 60612<br />312.243.8230</p>
           <p>Ops 3 LLC is an Illinois Limited Liability Company. Ops 3 has other assumed business names, including: Show Department, Resolution Digital Studios, The Screen Works, and Screen Source.</p>
           <p><strong>Trademarks:</strong> We claim a Service Mark for Resolution Productions Group (as a whole, see below) and Screen Source. Ops 3 LLC has registered various marks or variants, and logos, with the US Patent and Trademark Office, including: Show Department, Resolution Digital Studios, and The Screen Works. Our registered design trademarks include:</p>
           <p style="text-align: center; margin: 40px 0;">
@@ -28,7 +28,7 @@
           <p><strong>Privacy:</strong> We "collect" the information provided through our "contact" interface or which is otherwise provided to us. We will use this information to interact with you. We also collect analytical information concerning website traffic, which is generally anonymous.</p>
           <p>We do not sell this information. It used is solely to provide business service or information, or in enhancing your website experience and our internet presence. Disclosure under any circumstances to third parties will be limited to disclosures required by law or disclosures required by the services you have requested.</p>
           <p>If you are considering engaging in a business relationship with us, and you have privacy or disclosure concerns, please let us know. We take precautions to protect the identity of our clients, and the content they wish to produce. Please refer to the Terms and Conditions which will be supplied to you with a Proposal or Quote. Of course, when you enjoy having your name splashed across our social media networks, we are happy to oblige whenever the project lends itself to that activity.</p>
-          <p>Thank you for visiting our site.<br />Ellen Parker, Sr. Vice President-Legal, HR <a href="mailto:eparker@ops3llc.com" style="color: #655c58;">eparker@ops3llc.com</a></p>
+          <p>Thank you for visiting our site.<br />Ellen Parker<br />Sr. Vice President-Legal, HR<br /><a href="mailto:eparker@ops3llc.com" style="color: #655c58;">eparker@ops3llc.com</a></p>
         </div>
       </div>
     </div>
